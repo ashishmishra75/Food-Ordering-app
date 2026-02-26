3cf9a7e337d57e7b4713bf7d24774e6628737a4e
Ashish Mishra <ashishmishra0366@gmail.com>
2026-02-26 10:45:47 -0800

    Implemented nested routing structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,17 @@
 import { LOGO_URL } from "../utils/contants";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  // if no depen. array => useeffect is called on every render
+  // if depen. array is empty= [] => usEffect is called on intial render , just once
+  // if depeen. array is [btnName] => called everytime btnname is update
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnName]);
+
   return (
     <div className="header">
       <div>
@@ -12,6 +23,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
