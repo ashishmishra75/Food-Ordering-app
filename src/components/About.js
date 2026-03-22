9d5734d866512b2021fe9ec2993a75ac04780e6d
Ashish Mishra <ashishmishra0366@gmail.com>
2026-03-22 11:17:20 -0700

    Update the About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./user";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is Namaste React</h2>
+
+      <UserClass name={"akshay saini(class)"} />
     </div>
   );
 };
