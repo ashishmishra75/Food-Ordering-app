71ca81c0270e4ad7d53149f0633367925b4d5b34
Ashish Mishra <ashishmishra0366@gmail.com>
2026-03-22 20:58:31 -0700

    Removed unused API files

--- src/utils/api.js
+++
@@ -1,6 +0,0 @@
-export const SWIGGY_BASE_URL = "https://www.swiggy.com/dapi";
-
-export const PROXY_BASE_URL = "https://corsproxy.io/?";
-
-export const buildProxyUrl = (targetUrl) =>
-  `${PROXY_BASE_URL}${encodeURIComponent(targetUrl)}`;
