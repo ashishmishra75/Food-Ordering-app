3c138c484ebb14a198fb93f8866b0a684be3f6be
Ashish Mishra <ashishmishra0366@gmail.com>
2026-03-22 21:07:23 -0700

    inital changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"; //
 import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
-  const [resInfo, setresInfo] = useState(null);
+  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams(); //  URL se id aayegi automatically
 
   useEffect(() => {
     fetchMenu();
@@ -10,35 +12,46 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://corsproxy.io/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=608598&catalog_qa=undefined&submitAction=ENTER",
+      //  resId dynamic hai - har restaurant ke liye kaam karega
+      `https://corsproxy.io/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`,
     );
     const json = await data.json();
     console.log(json);
-    setresInfo(json.data);
+    setResInfo(json.data);
   };
 
   if (resInfo === null) return <Shimmer />;
 
+  const resInfoCard = resInfo?.cards?.find((c) => c?.card?.card?.info);
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[2]?.card?.card?.info;
+    resInfoCard?.card?.card?.info || {};
 
-  const { itemCards } =
-    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-
-  console.log(itemCards);
+  const menuCategories =
+    resInfo?.cards
+      ?.find((c) => c?.groupedCard?.cardGroupMap?.REGULAR)
+      ?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((c) =>
+        c?.card?.card?.["@type"]?.includes("ItemCategory"),
+      ) || [];
 
   return (
     <div className="menu">
       <h1>{name}</h1>
       <p>
-        {cuisines.join(", ")}, {costForTwoMessage}
+        {cuisines?.join(", ")}, {costForTwoMessage}
       </p>
       <h3>Menu</h3>
       <ul>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} -{"Rs."} {item.card.info.defaultPrice / 100}
-          </li>
+        {menuCategories.map((category, index) => (
+          <div key={index}>
+            <h4>{category?.card?.card?.title}</h4>
+            {category?.card?.card?.itemCards?.map((item) => (
+              <li key={item?.card?.info?.id}>
+                {item?.card?.info?.name} - Rs.{" "}
+                {(item?.card?.info?.price || item?.card?.info?.defaultPrice) /
+                  100}
+              </li>
+            ))}
+          </div>
         ))}
       </ul>
     </div>
