eaf7602a1590af8e74359e8a72aaaa9876a33201
Ashish Mishra <ashishmishra0366@gmail.com>
2026-03-28 07:58:28 -0700

    add css on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,26 +17,26 @@ const Header = () => {
   }, [btnName]);
 
   return (
-    <div className="header">
-      <div>
-        <img className="logo" src={LOGO_URL}></img>
+    <div className="flex justify-between bg-pink-100 shadow-lg">
+      <div className="logo-container">
+        <img className="w-36" src={LOGO_URL}></img>
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>online Status: {onlineStatus ? "✅" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4 ">
+          <li className="px-4">online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login-btn"
             onClick={() => {
