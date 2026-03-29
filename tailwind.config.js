79c0859880e68ece0e8bfa2822244dd1fe53db64
Ashish Mishra <ashishmishra0366@gmail.com>
2026-03-28 21:33:22 -0700

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}", "./index.html"], // ✅ jsx add kiya
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
