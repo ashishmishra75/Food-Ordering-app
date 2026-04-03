7188883acd42f124c17a1627a059d03d9600bee5
Ashish Mishra <ashishmishra0366@gmail.com>
2026-04-03 11:10:25 -0700

    .

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,6 +1,11 @@
 const Grocery = () => {
   return (
-    <h1>Our Grocery Store!! That also contain lots of components and childs</h1>
+    (
+      <h1>
+        Our Grocery Store!! That also contain lots of components and childs
+      </h1>
+    ),
+    (<h2>your store</h2>)
   );
 };
 export default Grocery;
